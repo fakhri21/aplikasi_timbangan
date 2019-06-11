834128e31a174ebefac010ac100a053d4d53ae57
fakhri <fakhri.warnet@live.com>
2019-06-11 16:45:04 +0700

    full wp

--- akuntansi/application/helpers/check_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('BASEPATH')) exit('no file allowed');
-function logincheck(){
-	$current_user = wp_get_current_user();
-	if(!isset($current_user->user_login )){
-		redirect('failure/locked');
-	}
-}
